7e50ba1a3d7675dae687aa3a6cb1dd26489c6988
Andrew Card <mrsaltinecracker1@gmail.com>
2021-08-28 18:48:55 -0700

    Autonomous Vision Aim

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -23,7 +23,8 @@ public class Drivetrain extends SubsystemBase {
   private SpeedControllerGroup m_leftmotors;
   private SpeedControllerGroup m_rightmotors;
 
-  
+  private NetworkTableInstance m_limelight = NetworkTableInstance.getDefault();
+  private NetworkTable m_visionData = m_limelight.getTable("limelight");
 
   public Drivetrain() {
 
@@ -49,9 +50,59 @@ public class Drivetrain extends SubsystemBase {
 
   }
 
+  /**
+   * Autonomous Vision Drive
+   */
   public void visionDrive(){
 
+    double kpAim = -0.1;
+    double kpDistance = -0.1;
 
+    double min_command = 0.05;
+
+    double tv = m_visionData.getEntry("tv").getDouble(0.0); //Does the limelight have a target 1 or 0
+    double tx = m_visionData.getEntry("tx").getDouble(0.0); //Horizontal Offset from crosshair
+    double ty = m_visionData.getEntry("ty").getDouble(0.0); //Vertical Offset from crosshair
+
+    double xSpeed = 0;
+    double zRotation = 0;
+
+    if(tv != 0){
+
+      double horizontalError = -tx;
+      double distanceError = -ty;
+
+      if(tx > 1){
+        zRotation = kpAim * horizontalError - min_command; 
+      }
+      if(tx < 1){
+        zRotation = kpAim * horizontalError + min_command; 
+      }
+
+      xSpeed = kpDistance * distanceError;
+
+      comboDrive(xSpeed, zRotation);      
+
+    }
+    else{
+
+      zRotation = 0.3;
+
+    }
+
+  }
+
+  /**
+   * TeleOp Vision Drive
+   * 
+   * @param xSpeed
+   * @param zRotation
+   */
+  public void visionDrive(double xSpeed, double zRotation){
+
+    double tv = m_visionData.getEntry("tv").getDouble(0.0); //Does the limelight have a target 1 or 0
+    double tx = m_visionData.getEntry("tx").getDouble(0.0); //Horizontal Offset from crosshair
+    double ty = m_visionData.getEntry("ty").getDouble(0.0); //Vertical Offset from crosshair
 
   }
 
